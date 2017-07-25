9272c7348b65d3c9cc045f8f9cda5340be94cafb
Christian Calle <calle.christian@gmail.com>
2017-07-25 07:18:43 +0000

    add loading screen

--- js/load.js
+++ js/load.js
@@ -1,43 +1,57 @@
+var loadingText;
+
+
 var loadState = {
+    
+    init: function () {
+        
+         this.game.load.onFileComplete.add(fileComplete, this);
+         this.game.add.sprite(0,0,'menu');
+          loadingText = this.game.add.text(300,150, 'Loading...', { fill: '#ffffff', fontSize: '60px' });
+         this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+  
+        this.game.scale.setScreenSize( true );
+        
+    },
     
     preload: function () {
         
         
         
-        game.load.image('diamond', 'assets/diamond.png');
-        game.load.image('sky', 'assets/sky.png');
-        game.load.image('ground', 'assets/platform5.png');
-        game.load.image('bubble', 'assets/bubble.png');
-        game.load.image('panel', 'assets/panel.png');
-        game.load.spritesheet('dude', 'assets/robo1.png', 48, 64);
-        game.load.image('menu', 'assets/MenuScreen.png');
-        game.load.image('startbutton', 'assets/startbutton.png');
-        game.load.image('star', 'assets/star.png');
-        game.load.image('directions', 'assets/directions.png');
-        game.load.image('playagain', 'assets/playagainbutton.png');
-        game.load.image('movebutton', 'assets/invisiblebutton.png');
-        game.load.image('settings', 'assets/settingsbutton.png');
-        game.load.image('settingspanel', 'assets/settingspanel.png');
-        game.load.image('submit', 'assets/submitbutton.png');
-        game.load.image('clear', 'assets/clearbutton.png');
+        this.game.load.image('diamond', 'assets/diamond.png');
+        this.game.load.image('sky', 'assets/sky.png');
+        this.game.load.image('ground', 'assets/platform5.png');
+        this.game.load.image('bubble', 'assets/bubble.png');
+        this.game.load.image('panel', 'assets/panel.png');
+        this.game.load.spritesheet('dude', 'assets/robo1.png', 48, 64);
+        this.game.load.image('menu', 'assets/MenuScreen.png');
+        this.game.load.image('startbutton', 'assets/startbutton.png');
+        this.game.load.image('star', 'assets/star.png');
+        this.game.load.image('directions', 'assets/directions.png');
+        this.game.load.image('playagain', 'assets/playagainbutton.png');
+        this.game.load.image('movebutton', 'assets/invisiblebutton.png');
+        this.game.load.image('settings', 'assets/settingsbutton.png');
+        this.game.load.image('settingspanel', 'assets/settingspanel.png');
+        this.game.load.image('submit', 'assets/submitbutton.png');
+        this.game.load.image('clear', 'assets/clearbutton.png');
     },
     
     create: function () {
         
-        game.add.sprite(0,0,'menu');
+        //this.game.add.sprite(0,0,'menu');
         
-        game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
   
-        game.scale.setScreenSize( true )
+        this.game.scale.setScreenSize( true );
         
-        var loadingText = game.add.text(300,150, 'Loading...', { fill: '#ffffff', fontSize: '60px' });
+       
         
-        var timerload = game.time.create(false);
+        //var timerload = game.time.create(false);
     
-        timerload.start();
+        //timerload.start();
         
     
-        timerload.add(2000, startMenu, this);
+        //timerload.add(2000, startMenu, this);
         
        
      //   game.state.start('menu');
@@ -54,6 +68,17 @@ var loadState = {
 
 function  startMenu () {
         
-        game.state.start('menu');
+        this.game.state.start('menu');
         
+}
+
+function fileComplete(progress, cacheKey, success, totalLoaded, totalFiles) {
+
+	loadingText.setText("Loading " + progress + "%");
+    if (progress == 100) {
+        
+        startMenu();
+        
+    }
+
 }
